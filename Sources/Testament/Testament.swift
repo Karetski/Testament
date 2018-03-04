abeb3d75f0dad50593bca50bf079a53a85f296c6
Alexey Karetski <karetski@gmail.com>
2018-03-04 03:14:18 +0300

    Basic unwrapping implementation

--- Sources/Testament/Testament.swift
+++ Sources/Testament/Testament.swift
@@ -1,3 +1,53 @@
-struct Testament {
-    var text = "Hello, World!"
+import XCTest
+
+protocol Message {
+    var text: String { get }
+}
+
+struct ExecutionLocation {
+    let file: StaticString
+    let line: UInt
+
+    static var empty: ExecutionLocation {
+        return .init(file: "Unknown", line: 0)
+    }
+}
+
+public struct Unwrapping<Type> {
+
+    private let instance: Type?
+    private let isCustomMessageApplied: Bool
+
+    public enum Error: Swift.Error {
+        case failedUnwrapping(Type.Type)
+    }
+
+    private struct FailureMessage: Message {
+        var text: String {
+            return "Failed to unwrap instance of " + String(describing: Type.self)
+        }
+    }
+
+    public init(_ instance: Type?, isCustomMessageApplied: Bool = false) {
+        self.instance = instance
+        self.isCustomMessageApplied = isCustomMessageApplied
+    }
+
+    public func make(file: StaticString = #file, line: UInt = #line) throws -> Type {
+        if let instance = instance {
+            return instance
+        } else {
+            if !isCustomMessageApplied {
+                testFail(
+                    message: FailureMessage(),
+                    executionLocation: ExecutionLocation(file: file, line: line)
+                )
+            }
+            throw Error.failedUnwrapping(Type.self)
+        }
+    }
+}
+
+func testFail(message: Message, executionLocation: ExecutionLocation) {
+    XCTFail(message.text, file: executionLocation.file, line: executionLocation.line)
 }
--- Tests/TestamentTests/TestamentTests.swift
+++ Tests/TestamentTests/TestamentTests.swift
@@ -3,12 +3,12 @@ import XCTest
 
 class TestamentTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Testament().text, "Hello, World!")
-    }
+        do {
+            let asd: String? = ""
+            let qwe = try Unwrapping(asd).make()
+        } catch { }
 
+    }
 
     static var allTests = [
         ("testExample", testExample),
