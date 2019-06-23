6ae06e550de7532d89cef8508c8836fdcdb2b4d3
Alexey Karetski <karetski@gmail.com>
2019-06-24 01:52:39 +0300

    Reimplement casting and unwrapping including tests

--- Sources/Testament/Casting.swift
+++ Sources/Testament/Casting.swift
@@ -2,38 +2,25 @@ public enum CastingError: Swift.Error {
     case unableToCast(instance: Any, type: Any.Type)
 }
 
-/// Structure that represents test of casting instance to `Type` with resulting casted value.
-public struct Casting<Type> {
-    private let instance: Any
-    private let isCustomAssertionApplied: Bool
-
-    /// Instantiates casting of `instance` to `Type`.
-    ///
-    /// - Parameters:
-    ///   - instance: Instance to be casted.
-    ///   - isCustomAssertionApplied: Flag that indicates need of custom test assertion. You shouldn't use it, generally.
-    public init(_ instance: Any, isCustomAssertionApplied: Bool = false) {
-        self.instance = instance
-        self.isCustomAssertionApplied = isCustomAssertionApplied
+/// Asserts that instance casts to type `Casted`, and returns casted value. Generates failure when `Initial` isn't casted to `Casted`.
+/// - Parameter instance: Instance to be casted.
+/// - Parameter to: Type of expected instance. *Defines the returning type.*
+/// - Parameter message: An optional description of the failure. If not provided uses default generated failure message. To silence the failure set to `nil`.
+/// - Parameter file: The file in which failure occurred. Defaults to the file name of the test case in which this function was called.
+/// - Parameter line: The line number on which failure occurred. Defaults to the line number on which this function was called.
+/// - Returns: A value of type `Casted`.
+/// - Throws: `CastingError.unableToCast` if type casting is unavailable.
+@discardableResult
+public func assertCasts<Initial, Casted>(
+    _ instance: Initial,
+    to: Casted.Type,
+    message: String? = "expected \(Initial.self) to be casted to \(Casted.self)",
+    file: StaticString = #file,
+    line: UInt = #line
+) throws -> Casted {
+    guard let casted = instance as? Casted else {
+        failIfNeeded(message, file: file, line: line)
+        throw CastingError.unableToCast(instance: instance, type: Casted.self)
     }
-
-    /// Tries to cast `instance` to `Type` if possible. Otherwise, throws an error.
-    ///
-    /// - Parameters:
-    ///   - file: File, where this function has been executed.
-    ///   - line: Line in file, where this function has been executed.
-    /// - Returns: Casted value of `Type`.
-    /// - Throws: `CastingError.unableToCast` if type casting is unavailable.
-    public func make(file: StaticString = #file, line: UInt = #line) throws -> Type {
-        guard let castedInstance = instance as? Type else {
-            Assertion.fail(
-                message: "Unable to cast instance to type \(Type.self)",
-                executionLocation: ExecutionLocation(file: file, line: line)
-            ).generate(
-                precondition: !isCustomAssertionApplied
-            )
-            throw CastingError.unableToCast(instance: instance, type: Type.self)
-        }
-        return castedInstance
-    }
+    return casted
 }
